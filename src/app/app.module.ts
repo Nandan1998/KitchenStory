097cec1af53e2252314cc3a4f90b6bd6fc6fecc1
Nandan1998 <raghunandanj007@gmail.com>
2022-12-03 13:21:06 +0530

    header & footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule, ReactiveFormsModule
